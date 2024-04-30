d0b7a76d397bf1427f0b4da1d6ac10368d8bd1cb
Arvind <arvindpardeshi007@gmail.com>
2024-04-30 15:37:02 +0530

    intial commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '8dbed8a7c8msh74a62156e231f4dp12aa9ejsn7e17659100b2',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (uuid) => createRequest(`/coin/${uuid}`),
+    }),
+    
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    getCryptoHistory: builder.query({
+      query: ({ uuid, timeperiod }) => createRequest(`coin/${uuid}/history?timeperiod=${timeperiod}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
+  
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
+
+
